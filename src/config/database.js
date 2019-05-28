adec136e0292f42a8f868b44c7182418e891d3d5
Thiago Giammattey <giammattey@gmail.com>
2019-05-27 22:13:22 -0300

    adding user registration

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'docker',
+  password: 'docker',
+  database: 'meetapp',
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+}
